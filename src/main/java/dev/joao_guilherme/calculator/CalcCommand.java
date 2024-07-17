dbf799104131d85b07a7b14502e02bb1ba6eed70
João Guilherme <jaaumgui@gmail.com>
2024-07-17 20:38:03 -0300

    refactor(CalcCommand): enhanced messages

--- src/main/java/dev/joao_guilherme/calculator/CalcCommand.java
+++ src/main/java/dev/joao_guilherme/calculator/CalcCommand.java
@@ -2,6 +2,7 @@ package dev.joao_guilherme.calculator;
 
 import dev.joao_guilherme.Expression;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,12 +19,13 @@ public class CalcCommand implements CommandExecutor, TabExecutor {
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if (commandSender instanceof Player player) {
             try {
-                player.sendMessage(Component.text(new Expression(String.join(" ", strings)).evaluate().toPlainString()));
+                String expression = String.join(" ", strings);
+                player.sendMessage(Component.text(expression + " = " + new Expression(expression).evaluate().toPlainString()).color(NamedTextColor.GREEN));
             } catch (Exception e) {
-                player.sendMessage(Component.text("Erro ao calcular a expressão!"));
+                player.sendMessage(Component.text("Erro ao calcular a expressão!").color(NamedTextColor.RED));
             }
         } else {
-            commandSender.sendMessage("Você precisa ser um jogador para usar este comando!");
+            commandSender.sendMessage(Component.text("Você precisa ser um jogador para usar este comando!").color(NamedTextColor.RED));
         }
         return true;
     }
