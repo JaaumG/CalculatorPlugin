fa7d8a398ea9543c62e704db8c7b848f04002b32
João Guilherme <jaaumgui@gmail.com>
2024-07-17 20:08:10 -0300

    Project Initialized

---
+++ src/main/java/dev/joao_guilherme/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package dev.joao_guilherme.calculator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Calculator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
