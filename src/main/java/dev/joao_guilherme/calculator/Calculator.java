54ebce2a4ce796ecb358305c580ec2fbac9752f3
João Guilherme <jaaumgui@gmail.com>
2024-07-17 20:34:21 -0300

    feat(Calculator): initialized command

--- src/main/java/dev/joao_guilherme/calculator/Calculator.java
+++ src/main/java/dev/joao_guilherme/calculator/Calculator.java
@@ -1,13 +1,16 @@
 package dev.joao_guilherme.calculator;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class Calculator extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage("[Calculator] Plugin initialized!");
+        Objects.requireNonNull(getCommand("calc")).setExecutor(new CalcCommand());
     }
 
     @Override
