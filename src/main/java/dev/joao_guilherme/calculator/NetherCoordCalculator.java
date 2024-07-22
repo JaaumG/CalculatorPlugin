32c3ab788b25801fba628061270438df2fef3be2
João Guilherme <jaaumgui@gmail.com>
2024-07-22 10:41:08 -0300

    refactor(NetherCoordCalculator): melhorado mensagens e listando coordenada do jogador

--- src/main/java/dev/joao_guilherme/calculator/NetherCoordCalculator.java
+++ src/main/java/dev/joao_guilherme/calculator/NetherCoordCalculator.java
@@ -1,31 +1,48 @@
 package dev.joao_guilherme.calculator;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class NetherCoordCalculator implements CommandExecutor {
+import java.util.List;
+
+public class NetherCoordCalculator implements CommandExecutor, TabExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if (commandSender instanceof Player player) {
             if (strings.length == 0) {
-                player.sendMessage("As coordenadas no Nether são: " + player.getLocation().getBlockX() / 8 + " " + player.getLocation().getBlockY() + " " + player.getLocation().getBlockZ() / 8);
-            } else if (strings.length == 3) {
+                player.sendMessage(Component.text("As coordenadas no Nether são: " + player.getLocation().getBlockX() / 8 + " " + player.getLocation().getBlockZ() / 8).color(NamedTextColor.AQUA));
+            } else if (strings.length == 2) {
                 try {
-                    int x = Integer.parseInt(strings[0]);
-                    int z = Integer.parseInt(strings[1]);
-                    int y = Integer.parseInt(strings[2]);
-                    player.sendMessage("As coordenadas no Nether são: " + x / 8 + " " + y + " " + z / 8);
+                    double x = Double.parseDouble(strings[0]);
+                    double z = Double.parseDouble(strings[1]);
+                    player.sendMessage(Component.text("As coordenadas no Nether são: " + x / 8 + " " + z / 8).color(NamedTextColor.AQUA));
                 } catch (NumberFormatException e) {
-                    player.sendMessage("As coordenadas precisam ser números inteiros!");
+                    player.sendMessage(Component.text("Coordenadas inválidas!").color(NamedTextColor.RED));
                 }
             } else {
-                player.sendMessage("Uso correto: /nethercoord <x> <z> <y>");
+                player.sendMessage(Component.text("Uso correto: /nethercoord <x> <z>").color(NamedTextColor.RED));
             }
         }
         return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (commandSender instanceof Player player) {
+            if (strings.length == 1) {
+                return List.of(String.valueOf(player.getLocation().getBlockX()));
+            } else if (strings.length == 2) {
+                return List.of(String.valueOf(player.getLocation().getBlockZ()));
+            }
+        }
+        return List.of();
     }
 }
